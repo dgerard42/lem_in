645d42978bbd2ed3b46d409535ac8769e876514f
Daniella Gerard <daniella4422@gmail.com>
2018-01-29 11:38:45 -0800

    tfw no bfs (breadth first search)

--- test_fts.c
+++ test_fts.c
@@ -21,9 +21,11 @@ void			check_inputs(t_swarm *swarm)
 	while (colony_ptr != NULL)
 	{
 		ft_printf("ROOM NAME: %s\n", colony_ptr->name);
+		ft_printf("type = %d, x = %d, y = %d\n", colony_ptr->room_type, colony_ptr->x_coord, colony_ptr->y_coord);
 		while (colony_ptr->tunnels != NULL)
 		{
 			ft_printf("TUNNEL FROM %s to %s\n", colony_ptr->name, ((colony_ptr->tunnels)->to_room)->name);
+			ft_printf("length = %d\n", (colony_ptr->tunnels)->length);
 			colony_ptr->tunnels = (colony_ptr->tunnels)->next;
 		}
 		colony_ptr = colony_ptr->next;
